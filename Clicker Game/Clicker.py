23f1c25446ebff369d160776a9cd3806e9d8c0d0
Bam-Code72 <bamiajayi72@gmail.com>
2025-08-23 15:40:39 +0100

    Clicker.py

--- Clicker Game/Clicker.py
+++ Clicker Game/Clicker.py
@@ -3,14 +3,29 @@ WIDTH=400
 HEIGHT=400
 #creating an actor
 target=Actor("target.png")
+mesg1="Welcome to the Target Game!"
 def move():
     x=random.randint(45,355)
     y=random.randint(45,355)
     target.pos=x,y
+    clock.schedule_unique(miss,0.6)
+def miss():
+    global mesg1
+    mesg1="You missed the target"
+    move()
 def draw():
     screen.fill("light blue")
-    screen.draw.text("Welcome to the Target Game!",(80,20),color="red")
+    screen.draw.text(mesg1,(80,20),color="red")
     #drawing the actor
     target.draw()
+#clicking of the mouse
+def on_mouse_down(pos):
+    global mesg1
+    if target.collidepoint(pos):
+        move()
+        mesg1="You hit the target!"
+        clock.unschedule(miss)
+    else:
+        mesg1="You missed the target!"
 move()
 pgzrun.go()
