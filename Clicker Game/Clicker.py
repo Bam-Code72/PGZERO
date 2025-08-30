9c41c483072e75b2e0343b710c4d79a198a3a18a
Bam-Code72 <bamiajayi72@gmail.com>
2025-08-30 15:09:42 +0100

    Add files via upload

--- Clicker Game/Clicker.py
+++ Clicker Game/Clicker.py
@@ -4,10 +4,11 @@ HEIGHT=400
 #creating an actor
 target=Actor("target.png")
 mesg1="Welcome to the Target Game!"
+score=0
 def move():
     x=random.randint(45,355)
     y=random.randint(45,355)
-    target.pos=x,y
+    target.pos=(x,y)
     clock.schedule_unique(miss,0.6)
 def miss():
     global mesg1
@@ -16,14 +17,16 @@ def miss():
 def draw():
     screen.fill("light blue")
     screen.draw.text(mesg1,(80,20),color="red")
+    screen.draw.text("Score:"+str(score),(10,10),color="red")
     #drawing the actor
     target.draw()
 #clicking of the mouse
 def on_mouse_down(pos):
-    global mesg1
+    global mesg1,score
     if target.collidepoint(pos):
         move()
         mesg1="You hit the target!"
+        score=score+10
         clock.unschedule(miss)
     else:
         mesg1="You missed the target!"
