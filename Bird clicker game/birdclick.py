76e0801e8a9956e67518d27fe771d5ee21050f71
Bam-Code72 <bamiajayi72@gmail.com>
2025-09-27 15:45:35 +0100

    birdclick.py

--- Bird clicker game/birdclick.py
+++ Bird clicker game/birdclick.py
@@ -4,22 +4,48 @@ HEIGHT=400
 listb=[]
 bnum=8
 lista=[]
+birdcount=0
+startime=0
+totaltime=0
 def birds():
-    global bnum
+    global bnum,startime
     for i in range(bnum):
         bird=Actor("bird image.png")
-        x=random.randint(20,680)
-        y=random.randint(20,380)
+        x=random.randint(30,680)
+        y=random.randint(30,380)
         bird.pos=(x,y)
         listb.append(bird)
+    #starting the timer
+    startime=time.time()
 def draw():
+    global birdcount,bnum,totaltime,startime
     sqnum=1
     screen.blit("cartoon field.jpg",(0,0))
     for a in listb:
         a.draw()
         screen.draw.text(str(sqnum),(a.pos[0],a.pos[1]+20),color="blue")
         sqnum+=1
-def mouse_down():
-
+    for b in lista:
+        screen.draw.line(b[0],b[1],"red")
+    #showing timer on the screen
+    if birdcount<bnum:
+        totaltime=time.time()-startime
+        totaltime=round(totaltime,1)
+        screen.draw.text("Time:"+str(totaltime),(20,20),color="red")
+    else:
+        screen.fill("black")
+        screen.draw.text("Your total time was:"+str(totaltime),(120,160),color="red",fontsize=60)
+def on_mouse_down(pos):
+    global birdcount,bnum,lista,listb
+    if birdcount<bnum:
+        if listb[birdcount].collidepoint(pos):
+            if birdcount!=0:
+                lista.append((listb[birdcount-1].pos,listb[birdcount].pos))
+            birdcount=birdcount+1
+        else:
+            lista=[]
+            birdcount=0
+def update():
+    pass
 birds()
 pgzrun.go()
