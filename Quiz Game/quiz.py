909a5db515f2670691278c30659980af5d8425aa
Bam-Code72 <bamiajayi72@gmail.com>
2025-10-11 15:59:04 +0100

    quiz.py

--- Quiz Game/quiz.py
+++ Quiz Game/quiz.py
@@ -10,9 +10,17 @@ ansbox2=Rect(270,240,250,150)
 ansbox3=Rect(10,400,250,150)
 ansbox4=Rect(270,400,250,150)
 skipbox=Rect(540,240,140,320)
+options=[ansbox1,ansbox2,ansbox3,ansbox4]
+#variables
+timer=15
+score=0
+over=False
+quest=[]
+index=0
+count=0
 def draw():
     screen.fill("black")
-    screen.draw.filled_rect(marbox,"light blue")
+    screen.draw.filled_rect(marbox,"black")
     screen.draw.filled_rect(quesbox,"green")
     screen.draw.filled_rect(timebox,"orange")
     screen.draw.filled_rect(ansbox1,"orange")
@@ -24,11 +32,51 @@ def draw():
     message="Welcome to the Country Quiz Game!"
     screen.draw.textbox(message,marbox,color="red")
     screen.draw.textbox("Skip",skipbox,color="black",angle=-90)
+    screen.draw.textbox(str(timer),timebox,color="black")
+    screen.draw.textbox(question[0].strip(),quesbox,color="red")
+    count2=1
+    for i in options:
+        screen.draw.textbox(question[count2].strip(),i,color="black")
+        count2=count2+1
 def movebox():
     marbox.x-=3
     if marbox.right<0:
         marbox.left=700
+def correct():
+    global timer,question,score,quest
+    score=score+50
+    if len(quest)>0:
+        question=readquest()
+        timer=15
+def on_mouse_down(pos):
+    ind=1
+    for i in options:
+        if i.collidepoint(pos):
+            if ind==int(question[5]):
+                correct()
+        ind+=1
+def timechange():
+    global timer
+    if timer!=0:
+        timer=timer-1
+#function to read the file
+def fileread():
+    global count,quest
+    file=open("question.txt","r")
+    for i in file:
+        quest.append(i)
+        count=count+1
+    file.close()
+#reading next question
+def readquest():
+    global index
+    index=index+1
+    return quest.pop(0).split(",")
 def update():
     pass
     movebox()
+#scheduling timer function call
+clock.schedule_interval(timechange,1)
+fileread()
+question=readquest()
 pgzrun.go()
