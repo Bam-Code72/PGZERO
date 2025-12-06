667576a3f9d5b877ce8e7ecc6a3e7b39b9a06bc6
Bam-Code72 <bamiajayi72@gmail.com>
2025-12-06 15:50:29 +0000

    fall.py

--- fall.py
+++ fall.py
@@ -4,23 +4,40 @@ HEIGHT=600
 #gaming variables
 score=0
 gam0=False
+gamW=False
 level=1
 levtotal=7
-speed=3
-fruitlist=["apple1.png","banana1.png","blueberry1.png","orange.png","pear1.png"]
+speed=10
+fruitlist=["apple1.png","banana1.png","blueberry1.png","orange1.png","pear1.png"]
 fall=[]
 targetitem=None
 def draw():
     screen.blit("farmbg.webp",(0,0))
+    if gam0==True:
+        screen.fill("black")
+        screen.draw.text("You Lose!,Your level:"+str(level),(300,350),fontsize=35,color="red")
+    elif gamW==True:
+        screen.fill("black")
+        screen.draw.text("You Win!,You completed all levels!",(300,350),fontsize=35,color="green")
+    else:
+
+        for i in fall:
+            i.draw()
+        screen.draw.text("Level "+str(level)+":",(30,30),fontsize=35,color="red")
+        screen.draw.text(f"Click the {targetitem.split('.')[0].capitalize()}!",(320,30),fontsize=40,color="blue")
 def update():
-    pass
+    global fall,level,gam0,gamW
+    if gam0 or gamW:
+        return
+    if len(fall)==0:
+        fall=drop(level)
 #functions for falling items
 def drop(extra):
     global targetitem
     minilist=[]
     targetitem=random.choice(fruitlist)
     wrongitem=random.choices([i for i in fruitlist if i != targetitem],k=extra)
-    complist=[targetitem]+[wrongitem]
+    complist=[targetitem]+wrongitem
     random.shuffle(complist)
     space=WIDTH/(len(complist)+1)
     for i,img in enumerate(complist):
@@ -30,11 +47,28 @@ def drop(extra):
         Actor1.y=0
         minilist.append(Actor1)
         animate(Actor1,duration=max(1,speed-level),on_finished=lambda a=Actor1:groundcheck(a),y=HEIGHT)
+    return minilist
 def groundcheck(actor):
     global gam0,targetitem
     if not actor.active:
         return
     if actor.image==targetitem:
         gam0=True
-
+def GO():
+    global gam0
+    gam0=True
+def on_mouse_down(pos):
+    global fall,level,gam0,gamW
+    for i in fall:
+        if i.collidepoint(pos):
+            if i.image==targetitem:
+                if level==levtotal:
+                    gamW=True
+                else:
+                    level+=1
+                    for a in fall:
+                        a.active=False
+                    fall=[]
+            else:
+                GO()
 pgzrun.go()
